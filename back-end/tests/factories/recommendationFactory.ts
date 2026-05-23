00d9504b92ac951be0465014458988368d50ed5e
Manuella Dias Cruz <manuelladcsilvacruz@gmail.com>
2026-05-22 22:52:05 -0300

    test: replace missing youtube helper with deterministic fixtures

--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -2,20 +2,17 @@ import { faker } from "@faker-js/faker";
 import { prisma } from "../../src/database.js";
 
 export interface Song {
-    name: string;
-    youtubeLink: string;
-};
+  name: string;
+  youtubeLink: string;
+}
 
-export function createRandomSong() {
-  const randomUrlGen = require("random-youtube-music-video");
-  const youtubeUrl = randomUrlGen.getRandomMusicVideoUrl();
-
-  const name = faker.name.findName();
-  const youtubeLink = youtubeUrl;
-
-  return { name, youtubeLink };
-};
+export function createRandomSong(): Song {
+  return {
+    name: faker.person?.fullName?.() ?? faker.name.findName(),
+    youtubeLink: `https://www.youtube.com/watch?v=${faker.string?.alphanumeric?.(11) ?? faker.random.alphaNumeric(11)}`,
+  };
+}
 
 export async function createRecommendation(song: Song) {
-    return prisma.recommendation.create({ data: song });
-};
+  return prisma.recommendation.create({ data: song });
+}
