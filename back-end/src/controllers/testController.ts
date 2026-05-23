38eed4f1973f8634a7fd2a499bf04c480b11c103
Manuella Dias Cruz <manuelladcsilvacruz@gmail.com>
2026-05-22 22:49:54 -0300

    fix: use esm-safe import in test controller

--- back-end/src/controllers/testController.ts
+++ back-end/src/controllers/testController.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
-import { testService } from "../services/testService";
+import { testService } from "../services/testService.js";
 
-export async function reset(req: Request, res: Response) {
-    await testService.deleteData();
-
-    res.sendStatus(200);
+export async function reset(_req: Request, res: Response) {
+  await testService.deleteData();
+  res.sendStatus(200);
 }
