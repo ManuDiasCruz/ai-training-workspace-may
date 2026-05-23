ed216a9c540b47633d88b42a6367018f6dfabed9
Manuella Dias Cruz <manuelladcsilvacruz@gmail.com>
2026-05-22 22:50:36 -0300

    fix: expose random recommendation fetch errors

--- front-end/src/hooks/api/useRecommendation.js
+++ front-end/src/hooks/api/useRecommendation.js
@@ -3,16 +3,17 @@ import useAsync from "../useAsync";
 import * as service from "../../services/recommendations";
 
 export default function useRecommendation() {
-  const { data, loading, act } = useAsync(service.get);
+  const { data, loading, error, act } = useAsync(service.get);
 
-  const update = (id) => {
-    act(id);
+  const update = () => {
+    act();
   };
 
   return {
     recommendation: data,
     loadingRecommendation: loading,
+    recommendationError: error,
     getRecommendation: act,
-    updateRecommendation: update
+    updateRecommendation: update,
   };
 }
